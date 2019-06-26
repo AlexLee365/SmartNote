8feab75e9c6f076943f06c773d81c291816693f1
Alex <kjj9647@gmail.com>
2019-06-26 17:43:51 +0900

    merge contents modifying

--- SmartNote/Controller/MemoListViewController.swift
+++ SmartNote/Controller/MemoListViewController.swift
@@ -2,10 +2,9 @@
 //  MemoListViewController.swift
 //  SmartNote
 //
-//  Created by Solji Kim on 26/06/2019.
+//  Created by 행복한 개발자 on 26/06/2019.
 //  Copyright © 2019 Alex Lee. All rights reserved.
 //
-
 import UIKit
 
 class MemoListViewController: UIViewController {
--- SmartNote/View/Cell/MemoListCell.swift
+++ SmartNote/View/Cell/MemoListCell.swift
@@ -2,7 +2,7 @@
 //  MemoListCell.swift
 //  SmartNote
 //
-//  Created by Solji Kim on 26/06/2019.
+//  Created by 행복한 개발자 on 26/06/2019.
 //  Copyright © 2019 Alex Lee. All rights reserved.
 //
 
@@ -49,7 +49,7 @@ class MemoListCell: UITableViewCell {
         noteIcon.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
         noteIcon.heightAnchor.constraint(equalToConstant: 50).isActive = true
         noteIcon.widthAnchor.constraint(equalToConstant: 50).isActive = true
-
+        
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor, constant: -10).isActive = true
         titleLabel.leadingAnchor.constraint(equalTo: noteIcon.trailingAnchor, constant: 15).isActive = true
