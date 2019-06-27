85b1203ab29aee845a5670b22e5c6841d8a8409f
doyeongkim <doyeong.kim1219@gmail.com>
2019-06-27 19:31:05 +0900

    detailVC update

--- SmartNote/Controller/MemoListViewController.swift
+++ SmartNote/Controller/MemoListViewController.swift
@@ -13,10 +13,25 @@ class MemoListViewController: UIViewController {
     let searchController = UISearchController(searchResultsController: nil)
     let tableView = UITableView()
     
+    var memoArray = [MemoData]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
         setAutoLayout()
+        makeData()
+    }
+    
+    func makeData() {
+        let date = Date()
+        memoArray = [
+            MemoData(date: date, text: "청와대는 영변 핵시설 전면 폐기가 북한 비핵화의 되돌릴 수 없는 단계라는 문재인 대통령의 어제 언급과 관련해 영변 핵폐기는 비핵화로 가기 위한 되돌릴 수 없는 단계로 접어드는 입구라고 설명했습니다. \n 청와대 핵심관계자는 오늘 기자들과 만나 어떤 사안을 보면 다시는 되돌릴 수 없는 정도의 것이 있지 않느냐면서 그것을 영변 핵폐기로 본다는 것이라며 이같이 말했습니다. \n 이 관계자는 그러면서 영변 핵폐기가 완전한 비핵화라는 게 아니라, 어느 단계를 되돌릴 수 없는 단계로 간주할 것인지가 협상의 핵심이 될 거라는 점이 대통령 인터뷰에 다 나와있다고 강조했습니다."),
+            
+            MemoData(date: date, text: "톱스타 부부인 송중기와 송혜교가 결혼한 지 1년8개월 만에 파경을 맞았습니다. 송중기는 법률대리인인 법무법인, 광장을 통해 어제 서울가정법원에 송혜교와의 이혼조정을 신청했다고 밝혔습니다. 이어 소속사를 통해 사생활에 대한 이야기들을 하나하나 말씀드리기 어려운 점 양해 부탁드리고, 앞으로 저는 지금의 상처에서 벗어나 연기자로서 작품 활동에 최선을 다하여 좋은 작품으로 보답하겠다고 밝혔습니다."),
+            
+            MemoData(date: date, text: "memo3"),
+            MemoData(date: date, text: "memo4")
+        ]
     }
     
     // shows search bar without scrolling up
@@ -72,16 +87,25 @@ class MemoListViewController: UIViewController {
 
 extension MemoListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return memoArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "MemoListCell", for: indexPath) as! MemoListCell
         
-        cell.titleLabel.text = "여행이좋아"
-        cell.dateLabel.text = "2019. 6. 1."
-        cell.descriptionLabel.text = "여행을 통해 누군가의 성장을 바라보며 어ㅓ어어어어어엉어"
+        let formatter: DateFormatter = {
+            let f = DateFormatter()
+            f.dateStyle = .medium
+            f.timeStyle = .none
+            f.locale = Locale(identifier: "ko")
+            return f
+        }()
+        
+        cell.accessoryType = .disclosureIndicator
+        cell.titleLabel.text = memoArray[indexPath.row].returnTitle()
+        cell.dateLabel.text = formatter.string(from: memoArray[indexPath.row].date)
+        cell.descriptionLabel.text = memoArray[indexPath.row].returnBody()
         cell.noteIcon.image = UIImage(named: "noteIcon")
         cell.selectionStyle = .none
         
@@ -113,6 +137,12 @@ extension MemoListViewController: UITableViewDataSource {
 // MARK: - UITableViewDelegate
 
 extension MemoListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let detailMemoVC = DetailMemoViewController()
+        navigationController?.pushViewController(detailMemoVC, animated: true)
+    }
     
 }
 
